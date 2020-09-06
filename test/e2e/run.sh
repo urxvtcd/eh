de64e45e3c82d3cdd81ca4c7c489c81633f457a2
Marcin Charęza <mc@surd.us>
2020-09-06 18:44:16 +0200

    Rework e2e test script
      - use custom function to wrap calls to sed
      - add set Eeuo pipefail
      - color input command string in test output
      - color lines telling about tests failures red
      - add comments

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -1,10 +1,28 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-runtest 2>&1 |
-    sed \
-        -e '/^expect: does "" (spawn_id/ s|^.*match glob pattern \(.*\)? [yesno]*\r|trying to match \x1b[33m\1\x1b[0m|' \
-        -e '/^expect: does "[^"].* no\r/ s|^[^"]*\("[^"]*"\).*|\x1b[31m\1 no\x1b[0m|' \
-        -e '/^expect: does "[^"].* yes\r/ s|^[^"]*\("[^"]*"\).*|\x1b[32m\1 yes\x1b[0m|' \
-        -e '/^expect: set / d' \
-        -e '/^send: sending / s|.*|\n&|' \
-        -e '/^\r/ d'
+set -Eeuo pipefail
+
+red="\x1b[31m"
+green="\x1b[32m"
+yellow="\x1b[33m"
+cyan="\x1b[36m"
+reset="\x1b[0m"
+
+function wsed {
+    # wsed wraps sed's substitute into a command. Wins:
+    #  - less repetition,
+    #  - skipping quote escaping game,
+    #  - easy vertical alignment.
+
+    sed "/${1}/ s/${2}/${3}/"
+}
+
+# run tests and make output easier on the eyes
+runtest 2>&1 \
+    | wsed "^send: "           '.*\("[^"]*"\).*'         "\nsending ${cyan}\1 ${reset}" \
+    | wsed '^expect: does ""'  '^.*pattern \(.*\)? .*\r' "trying to match ${cyan}\1${reset}" \
+    | wsed '^expect: .* no\r'  '^[^"]*\("[^"]*"\).*'     "${yellow}\1 no${reset}" \
+    | wsed '^expect: .* yes\r' '^[^"]*\("[^"]*"\).*'     "${green}\1 yes${reset}" \
+    | wsed '^FAIL: '           '.*'                      "${red}&${reset}" \
+    | sed -e '/^expect: set / d' \
+    | sed -e '/^\r/           d'
